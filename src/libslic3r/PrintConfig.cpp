a974ec2e7ca7c06eb0c8e2c53f60385cc6e1646e
aceRage <ace.savell@gmail.com>
2026-09-25 04:24:25 -0500

    Flatpak build: add missing boost::algorithm split/classification includes to PrintConfig.cpp
    Same latent bug as FillBedPack.cpp and ToolOrdering.cpp: boost::algorithm::split /
    boost::split and boost::algorithm::is_any_of / boost::is_any_of are used here but
    only available via the PCH's boost/algorithm/string.hpp, so the Flatpak build
    (SLIC3R_PCH off) failed once it got past the earlier two fixes.
    
    Deft

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -12,6 +12,8 @@
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/case_conv.hpp>
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <boost/format.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/log/trivial.hpp>
